910ba5b371fdbaef7195639ab6a75f153abf0164
kurtsys <kurt.sys@gmail.com>
2015-01-03 14:01:31 +0100

    change api - add pars

--- qsys/be/api.clj
+++ qsys/be/api.clj
@@ -5,20 +5,38 @@
 
 (def address "be.qsys.tinyleaps.api")
 
-(defmulti generateCall :action)
+(defmulti jdbcCall :action)
 
-(defmethod generateCall "countries" [msg] {:action "select" :stmt "select * from travelblog.country"})
-(defmethod generateCall :default [_] "")
+(defmethod jdbcCall "countries" [msg] 
+  {:action "select" 
+   :stmt "select * from travelblog.country"})
 
+(defmethod jdbcCall "blogs" [msg] 
+  (let [pars (:pars msg)
+        stmt (str "select bi.postdate, bi.title, bi.summary, bi.text "
+              "from travelblog.blogitem bi "
+              "where bi.id in "
+                "(select distinct r.blogitem"
+                " from travelblog.route r"
+                " where r.travel = ? and r.blogitem is not null) "
+              "order by bi.postdate desc "
+              "limit ? "
+              "offset ?")] 
+    { :action "select" 
+      :stmt stmt
+      :values [[(:travel pars) , (:limit pars), (:offset pars)]]}
+   
+  ))
+
+(defmethod jdbcCall :default [_] "")
 
 (defn send [addr msg]
   (let [ch (chan 1)]
     (eb/send addr msg #(put! ch %)) ch))
 
-
 (eb/on-message
   address
   (fn [msg]
-    (go (let [reply (<! (send "be.qsys.tinyleaps.postgresql" (generateCall msg)))]
+    (go (let [reply (<! (send "be.qsys.tinyleaps.postgresql" (jdbcCall msg)))]
           (eb/reply reply)))))
 
