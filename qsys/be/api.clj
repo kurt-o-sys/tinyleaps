976d0b87a58e8334e1fcc9b9ca4db47f15fb346c
kurtsys <kurt.sys@gmail.com>
2015-01-10 23:33:21 +0100

    update async call: remove unnecessary channel

--- qsys/be/api.clj
+++ qsys/be/api.clj
@@ -3,7 +3,7 @@
   (:require [ vertx.core :as core]
             [ vertx.eventbus :as eb]
             [ postgres.async :refer :all]
-            [ clojure.core.async :refer [go chan put! <! <!!]]))
+            [ clojure.core.async :refer [<!!]]))
  
 
 (def address "be.qsys.tinyleaps.api")
@@ -48,15 +48,9 @@
 (defmethod asyncCall :default [_] "")
 
 
-(defn getAsync [msg]
-  (let [ch (chan 1)]
-    (put! ch (asyncCall msg))))
-
-
 (eb/on-message
   address
   (fn [msg]
-    (go (let [reply (<! (getAsync msg))]
-          (eb/reply reply)))))
+    (eb/reply (getAsync msg))))
 
 (core/on-stop (close-db! db))
