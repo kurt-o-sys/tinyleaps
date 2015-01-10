0ecc2c1b83acfb10e8b95d2d32b547d328d99a18
kurtsys <kurt.sys@gmail.com>
2015-01-10 20:53:22 +0100

    change postgres async driver

--- qsys/be/api.clj
+++ qsys/be/api.clj
@@ -1,57 +1,60 @@
 (ns be.qsys.tinyleaps.api
   (:refer-clojure :exclude [send])
-  (:require [ vertx.eventbus :as eb]
+  (:require [ vertx.core :as core]
+            [ vertx.eventbus :as eb]
+            [ postgres.async :refer :all]
             [ clojure.core.async :refer [go chan put! <!]]))
 
 (def address "be.qsys.tinyleaps.api")
 
-(defmulti jdbcCall :action)
+(def db (open-db {:hostname "localhost"
+                  :port 5432
+                  :database "main"
+                  :username "kurt"
+                  :password "oebele"
+                  :pool-size 2 }))
 
-(defmethod jdbcCall "travelinfo" [msg] 
+(defmulti asyncCall :action)
+
+(defmethod asyncCall "travelinfo" [msg] 
   (let [pars (:pars msg)
         stmt (str "select ti.name as name, "
                   "extract(epoch from ti.startdate) as startdate, "
                   "extract(epoch from ti.enddate) as enddate, "
                   "array_to_string(ti.countries,',') as countries "
                   "from travelblog.travelinfo ti "
-                  "where ti.id = ?")]
-    {:action "select"
-     :stmt stmt
-     :values [[(:travel pars)]] }
-    ))
+                  "where ti.id = $1")]
+    (<!! (query! db [stmt (:travel pars)]))))
 
-
-(defmethod jdbcCall "countries" [msg] 
+(defmethod asyncCall "countries" [msg] 
   (let [stmt "select * from travelblog.country" ]
-    {:action "select" 
-     :stmt stmt}
-    ))
+    (<!! (query! db [stmt]))))
 
-(defmethod jdbcCall "blogs" [msg] 
+(defmethod asyncCall "blogs" [msg] 
   (let [pars (:pars msg)
         stmt (str "select extract(epoch from bi.postdate), bi.title, bi.summary, bi.text "
                   "from travelblog.blogitem bi "
                   "where bi.id in "
                   "(select distinct r.blogitem"
                   " from travelblog.route r"
-                  " where r.travel = ? and r.blogitem is not null) "
+                  " where r.travel = $1 and r.blogitem is not null) "
                   "order by bi.postdate desc "
-                  "limit ? "
-                  "offset ?")] 
-    {:action "select" 
-     :stmt stmt
-     :values [[(:travel pars) , (:limit pars), (:offset pars)]]}
-    ))
+                  "limit $2 "
+                  "offset $3")] 
+    (<!! (query! db [stmt (:travel pars) (:limit pars) (:offset pars)]))))
 
-(defmethod jdbcCall :default [_] "")
+(defmethod asyncCall :default [_] "")
 
-(defn send [addr msg]
+
+(defn getAsync [msg]
   (let [ch (chan 1)]
-    (eb/send addr msg #(put! ch %)) ch))
+    (put! ch (asyncCall msg))))
+
 
 (eb/on-message
   address
   (fn [msg]
-    (go (let [reply (<! (send "be.qsys.tinyleaps.postgresql" (jdbcCall msg)))]
+    (go (let [reply (<! (getAsync msg))]
           (eb/reply reply)))))
 
+(core/on-stop (closedb! db))
