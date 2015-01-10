b21b2d77eca87ad4396fa1d11b343cf4af25b67b
kurtsys <kurt.sys@gmail.com>
2015-01-10 20:53:22 +0100

    change postgres async driver

--- qsys/be/app.clj
+++ qsys/be/app.clj
@@ -3,13 +3,5 @@
 
 (let [cfg (config)]
   (deploy-verticle "qsys/be/api.clj"
-    :config (:api cfg))
-
-  (deploy-module "io.vertx~mod-web-server~2.0.0-final"
-    :config (:webserver cfg))
-
-  (deploy-module "com.bloidonia~mod-jdbc-persistor~2.1.2"
-    :config (:db cfg)
-    :instances 1))
-
+    :config (:api cfg)))
 
